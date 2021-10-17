d4709f982fbcb5c2be22a1f5ce6320874f957e91
Gerard Hynes <gkhynes@gmail.com>
2021-10-17 21:18:06 +0100

    Add joins challenge 2

--- exercises/tv_joins.sql
+++ exercises/tv_joins.sql
@@ -5,3 +5,13 @@ SELECT
 FROM series
 JOIN reviews
     ON series.id = reviews.series_id;
+
+-- Challenge 2
+SELECT
+    title,
+    AVG(rating) as avg_rating
+FROM series
+JOIN reviews
+    ON series.id = reviews.series_id
+GROUP BY series.id
+ORDER BY avg_rating;
