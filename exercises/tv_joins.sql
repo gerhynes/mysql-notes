82e05fe5fe15ddae88617b96203ed7ff0c6e4e6b
Gerard Hynes <gkhynes@gmail.com>
2021-10-18 21:39:25 +0100

    Add join challenge 5

--- exercises/tv_joins.sql
+++ exercises/tv_joins.sql
@@ -32,3 +32,12 @@ FROM series
 LEFT JOIN reviews
     ON series.id = reviews.series_id
 WHERE rating IS NULL;
+
+-- Challenge 5
+SELECT
+    genre,
+    ROUND(AVG(rating), 2) AS avg_rating
+FROM series
+INNER JOIN reviews
+    ON series.id = reviews.series_id
+GROUP BY genre;
