511f6bba3ef084566a113ab050fd3c818b269f6f
Gerard Hynes <gkhynes@gmail.com>
2021-10-25 21:22:11 +0100

    Add Instagram photos schema

--- instagram_clone/instagram_db.sql
+++ instagram_clone/instagram_db.sql
@@ -1,4 +1,4 @@
-DROP DATABASE instagram_db;
+-- DROP DATABASE instagram_db;
 CREATE DATABASE instagram_db;
 USE instagram_db;
 
@@ -8,7 +8,20 @@ CREATE TABLE users (
   created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE photos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  image_url VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
 INSERT INTO users (username) VALUES
 ('BlueTheCat'),
 ('CharlieBrown'),
 ('ColtSteele');
+
+INSERT INTO photos (image_url, user_id) VALUES
+('/asdfqwer', 1),
+('/hjgfjhuy', 2),
+('/uerfdoiy', 2);
