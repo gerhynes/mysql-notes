acfd8e0412507e8288a4ae90f52cb42484dc1429
Gerard Hynes <gkhynes@gmail.com>
2021-10-25 21:32:53 +0100

    Add Instagram comments schema

--- instagram_clone/instagram_db.sql
+++ instagram_clone/instagram_db.sql
@@ -16,6 +16,16 @@ CREATE TABLE photos (
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+CREATE TABLE comments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  comment_text VARCHAR(255) NOT NULL,
+  photo_id INT NOT NULL,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY(photo_id) REFERENCES photos(id),
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
 INSERT INTO users (username) VALUES
 ('BlueTheCat'),
 ('CharlieBrown'),
@@ -25,3 +35,8 @@ INSERT INTO photos (image_url, user_id) VALUES
 ('/asdfqwer', 1),
 ('/hjgfjhuy', 2),
 ('/uerfdoiy', 2);
+
+INSERT INTO comments (comment_text, user_id, photo_id) VALUES
+('Meow', 1, 2),
+('Amazing shot!', 3, 2),
+('I <3 this', 2, 1);
