95d2c6eb50f14df7fdc19e0b33d7d11614b01d53
Gerard Hynes <gkhynes@gmail.com>
2021-10-25 21:12:21 +0100

    Add Instagram users schema

---
+++ instagram_clone/instagram_db.sql
@@ -0,0 +1,14 @@
+DROP DATABASE instagram_db;
+CREATE DATABASE instagram_db;
+USE instagram_db;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO users (username) VALUES
+('BlueTheCat'),
+('CharlieBrown'),
+('ColtSteele');
