f153664ad595dbff46d4dfb421db3a45122c9f23
Gerard Hynes <gkhynes@gmail.com>
2021-10-27 21:15:30 +0100

    Add hashtag schema

--- instagram_clone/instagram_db.sql
+++ instagram_clone/instagram_db.sql
@@ -44,6 +44,20 @@ CREATE TABLE follows (
   PRIMARY KEY(follower_id, followee_id)
 );
 
+CREATE TABLE tags (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+  photo_id INT NOT NULL,
+  tag_id INT NOT NULL,
+  FOREIGN KEY(photo_id) REFERENCES photos(id),
+  FOREIGN KEY(tag_id) REFERENCES tags(id),
+  PRIMARY KEY(photo_id, tag_id)
+);
+
 -- Insert sample values
 
 INSERT INTO users (username) VALUES
@@ -75,3 +89,14 @@ VALUES
 (1,3),
 (3,1),
 (2,3);
+
+INSERT INTO tags (tag_name) VALUES
+('adorable'),
+('cute'),
+('sunrise');
+
+INSERT INTO photo_tags (photo_id, tag_id) VALUES
+(1,1),
+(1,2),
+(2,3),
+(3,1);
