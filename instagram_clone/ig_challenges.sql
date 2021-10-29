7a7c94480066bed9678ada8ad4d354613620b8b9
Gerard Hynes <gkhynes@gmail.com>
2021-10-29 16:39:00 +0100

    Add Instagram challenge 6

--- instagram_clone/ig_challenges.sql
+++ instagram_clone/ig_challenges.sql
@@ -37,3 +37,13 @@ LIMIT 1;
 -- 5. Calculate average number of photos per user
 SELECT 
     (SELECT Count(*) FROM photos) / (SELECT Count(*) FROM users) AS avg;
+
+-- 6. Find the five most popular hashtags
+SELECT tags.tag_name, 
+       COUNT(*) AS total 
+FROM photo_tags 
+    JOIN tags 
+    ON photo_tags.tag_id = tags.id 
+GROUP BY tags.id 
+ORDER BY total DESC 
+LIMIT 5; 
