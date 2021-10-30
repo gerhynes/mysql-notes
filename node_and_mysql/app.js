0e14a478f6dd13ebc447739fb3298ea3e3e84d81
Gerard Hynes <gkhynes@gmail.com>
2021-10-30 13:24:13 +0100

    Connect node app to mysql db

---
+++ node_and_mysql/app.js
@@ -0,0 +1,19 @@
+const faker = require("faker");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB
+});
+
+connection.connect();
+
+connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+  if (error) throw error;
+  console.log("The solution is: ", results[0].solution);
+});
+
+connection.end();
