03ba9e1c119e8a55dceb2ada871bab1ff6d4e7af
Gerard Hynes <gkhynes@gmail.com>
2021-10-31 12:21:56 +0000

    Bulk insert 500 random users

--- node_and_mysql/app.js
+++ node_and_mysql/app.js
@@ -11,15 +11,40 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-const person = { email: faker.internet.email(), created_at: faker.date.past() };
+// =======================
+// Inserting one user
+// =======================
 
-connection.query(
-  "INSERT INTO users SET ?",
-  person,
-  function (error, results, fields) {
-    if (error) throw error;
-    console.log(results);
-  }
-);
+// const person = { email: faker.internet.email(), created_at: faker.date.past() };
+
+// const end_result = connection.query(
+//   "INSERT INTO users SET ?",
+//   person,
+//   function (error, results, fields) {
+//     if (error) throw error;
+//     console.log(results);
+//   }
+// );
+
+// // View the SQL query you created
+// console.log(end_result.sql);
+
+// =======================
+// Bulk inserting 500 rows
+// =======================
+const data = [];
+
+for (let i = 0; i < 500; i++) {
+  data.push([faker.internet.email(), faker.date.past()]);
+}
+
+// Create query
+const q = "INSERT INTO users (email, created_at) VALUES ?";
+
+// Execute query
+connection.query(q, [data], function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
 
 connection.end();
